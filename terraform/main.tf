5d5ddef9386faba8c9875481ed350252e11e5ef5
Ubuntu <ubuntu@ip-172-31-11-39.ap-south-1.compute.internal>
2026-08-04 09:04:31 +0000

    Day 6: Add Public Route Table and Association

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,3 +43,30 @@ resource "aws_subnet" "public" {
 
 }
 
+# Public Route Table
+
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+
+    Name = "${var.project_name}-public-route-table"
+  }
+
+}
+
+# Route Table Association
+
+resource "aws_route_table_association" "public" {
+
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+
+}
